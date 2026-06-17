b2b6614e58e74d5afcc394bacd9aff7146efcbf0
Raghavray Parmar <raghavrayparmar@gmail.com>
2026-06-17 08:09:47 +0530

    Add testbench for NAND gate

---
+++ nand/nand_tb.v
@@ -0,0 +1,19 @@
+module nand_tb();
+reg a,b;
+wire y;
+
+nand_gate uut(
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    a=1'b0; b=1'b0;
+    #10 a=1'b0; b=1'b1;
+    #10 a=1'b1; b=1'b0;
+    #10 a=1'b1; b=1'b1;
+    #20 $finish;
+end
+
+endmodule
