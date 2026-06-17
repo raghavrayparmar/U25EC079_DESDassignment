4831362de0e485e5b0d3a3a016860ffcd527867f
Raghavray Parmar <raghavrayparmar@gmail.com>
2026-06-17 08:19:47 +0530

    Add testbench for XNOR gate

---
+++ xnor/xnor_tb.v
@@ -0,0 +1,19 @@
+module xnor_tb();
+reg a,b;
+wire y;
+
+xnor_gate uut(
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin 
+    a=1'b0; b=1'b0;
+    #10 a=1'b0; b=1'b1;
+    #10 a=1'b1; b=1'b0;
+    #10 a=1'b1; b=1'b1;
+    #20 $finish;
+end
+
+endmodule
